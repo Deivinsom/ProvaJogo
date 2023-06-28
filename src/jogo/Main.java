9906e83455056173e7cb29806eed9f3d59ab4ad9
Deivid Oliveira <deivid.oliveira00000@gmail.com>
2023-06-28 01:09:34 -0300

    Poder joia faltando bem poquin

--- src/jogo/Main.java
+++ src/jogo/Main.java
@@ -2,13 +2,12 @@ package src.jogo;
 
 public class Main {
     Maxwell max = new Maxwell(3, 7, 0);    
-    Joia joia = new Joia();
+    Joia joia = new Joia(0);
     public static void main(String[] args) {
         MapaGrafo.addCaminhos();
         MapaGrafo.addCidades();
-        Menu me = new Menu();
+        Menu menu = new Menu();
 
-        me.menuPrincipal();
+        menu.menuPrincipal();
     }
-
 }
